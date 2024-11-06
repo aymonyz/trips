042c6a41146bb2ff90b133cf5be05154b1b07670
aymonyz <aymonyz@yahoo.com>
2024-11-06 13:18:03 +0300

    transelat

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'ar',  // تعيين اللغة الافتراضية إلى العربية
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+    }
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
 <head>
     <meta charset="utf-8">
-    <title>Tourist - Travel Agency HTML Template</title>
+    <title>Guide Me | أرشدني</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
