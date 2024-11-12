093319da712596beb983e49333f7b36e4ce7322f
ahmedsaeed0 <ahmedsaeed2000.zkh@gmail.com>
2024-11-12 07:19:06 +0300

    last_editسسسسسسسسسسسسسسسسسسسسسسسسسسس

--- base_nav.php
+++ base_nav.php
@@ -118,7 +118,6 @@ if ($page == 'home') {
         
         </div>
         <?php if (isset($_SESSION['userId'])): ?>
-<<<<<<< HEAD
             <?php if ($_SESSION['role'] == 'user'): ?>
                     <a href="../index.php?page=profile" class="btn btn-primary rounded-pill py-2 px-4">الملف الشخصي</a>
                     <a href="../index.php?page=logout" class="btn btn-secondary rounded-pill py-2 px-4 ms-2">تسجيل خروج</a>
@@ -128,7 +127,7 @@ if ($page == 'home') {
         <?php else: ?>
             <!-- Show Register if not logged in -->        <?php endif; ?>
     </div>
-=======
+
     <!-- Show Profile and Logout if logged in -->
     <?php if ($_SESSION['role'] == 'user'): ?>
         <a href="../index.php?page=profile" class="btn btn-primary rounded-pill py-2 px-4">الملف الشخصي</a>
@@ -137,9 +136,8 @@ if ($page == 'home') {
         <a href="dashboard-button.php" class="btn btn-primary rounded-pill py-2 px-4">الملف الشخصي</a>
         <a href="../index.php?page=logout" class="btn btn-secondary rounded-pill py-2 px-4 ms-2">تسجيل خروج</a>
     <?php endif; ?>
-<?php endif; ?>
+<!-- #اخر تعديل  -->
 
->>>>>>> 7018f743231e2200caa2daaf23d49de95c297c74
 </nav>
 
 
